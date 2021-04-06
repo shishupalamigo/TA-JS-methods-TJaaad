1d3927c2cbb684da135368f2c79ed279918e7b47
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-04-07 00:41:53 +0530

    Refactor code | block-BJaadc | Assignment: String Methods - Block 3

--- block-BJaadc/code/index.js
+++ block-BJaadc/code/index.js
@@ -10,13 +10,13 @@ console.log(`The first character of ${message} is ${message.charAt(0)}`);
 
 // Log the last character of message using length like `The last character of [message] is [character]`
 
-console.log(`The first character of ${message} is ${message.charAt(message.length - 1)}`);
+console.log(`The last character of ${message} is ${message.charAt(message.length - 1)}`);
 
 // Log the index of word "you" in message
 
 console.log(message.indexOf("you"));
 
-// Log the index of word "still" in message
+//  Log the index of word "still" in message
 
 console.log(message.indexOf("still"));
 
@@ -42,32 +42,54 @@ console.log(message.includes("Is"));
 
 // Check and log whether the word from index 3 to 6 is "you" or not (use slice)
 
-console.log(message.slice(3, 6, "you"));
+console.log(message.slice(3, 6) === "you");
 
 // Check and log whether the word from index 7 to 8 is "a" or not
 
-console.log(message.slice(7, 8, "a"));
+console.log(message.slice(7, 8) === "a");
 
 // Check and log whether the last 3 character in message is "no!" or not (you can use -3 in slice)
 
-console.log(message.slice(40, -3, "no!"));
+console.log(message.slice(-3) === "no!");
 
 // Check and log whether the last 5 character in message is "no!" or not
 
-
+console.log(message.slice(-3) === "no!");
 
 // Log the message variable, all in lowecase
 
+console.log(message.toLowerCase());
+
 // Log the message variable, all in uppercase
+
+console.log(message.toUpperCase());
 
 // Replace the word "are" to "can't", store it in a new variable named `newMessage` and log it.
 
+let newMessage = message.replace("are", `can't`);
+console.log(newMessage);
 // Replace the word "still" to "" (empty) using newMessage variable, and re-assign the output to the variable `newMessage` and log it.
+
+newMessage = newMessage.replace("still", "");
+console.log(newMessage);
 
 // Replace the word "thinking" to "decide" using newMessage variable, and re-assign the output to the variable `newMessage` and log it.
 
+newMessage = newMessage.replace("thinking", "decide");
+console.log(newMessage);
+
 // Log all the characters from the message variable (you can user for..of loop on string)
 
-// Split all the words in message (split by " " space) and store it in a variable messageArray
+// for (let i = 0; i < message.length -1; i++) {
+//     console.log(message.charAt(i));
+// }
+for (let char of message) {
+    console.log(char);
+}
 
+// Split all the words in message (split by " " space) and store it in a variable messageArray
+let messageArray = message.split(" ");
 // Log all the words of messageArray
+for (let word of messageArray) {
+    console.log(word);
+}
