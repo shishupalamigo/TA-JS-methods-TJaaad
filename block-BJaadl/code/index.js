8060324e4235cd68c54dfd9fc96fe8636f6cb55e
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-04-10 22:42:18 +0530

    Add Solutions | block-BJaadl | Assignment Level II - Block 3

--- block-BJaadl/code/index.js
+++ block-BJaadl/code/index.js
@@ -11,19 +11,79 @@ let words = [
 
 // - Write a function findLongestWord that takes an array of words and returns the longest word from the array. (Use above array "words" to test it). If there are 2 with the same length, it should return the first occurrence.
 
+function findLongestWord(array) {
+  return [...array].sort((a, b) => b.length - a.length)[0];
+}
+findLongestWord(words);
+console.log(findLongestWord(words));
+
 // - Convert the above array "words" into an array of length of word instead of word.
+
+console.log(words.map(word => word.length));
 
 // - Create a new array that only contains word with atleast one vowel.
 
+let wordsWithVowel = words.filter((word) => {
+  if (
+    word.toLowerCase().includes("a") ||
+    word.toLowerCase().includes("e") ||
+    word.toLowerCase().includes("i") ||
+    word.toLowerCase().includes("o") ||
+    word.toLowerCase().includes("u")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+console.log(wordsWithVowel);
+
 // - Find the index of the word "rhythm"
+
+console.log(words.findIndex(word => word === "rhythm"));
 
 // - Create a new array that contians words not starting with vowel.
 
+let wordsStartingWithVowel = words.filter((word) => {
+  if (
+    word.toLowerCase().startsWith("a") ||
+    word.toLowerCase().startsWith("e") ||
+    word.toLowerCase().startsWith("i") ||
+    word.toLowerCase().startsWith("o") ||
+    word.toLowerCase().startsWith("u")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+console.log(wordsStartingWithVowel);
+
 // - Create a new array that contianse words not ending with vowel
+
+let wordsEndingWithVowel = words.filter((word) => {
+  if (
+    word.toLowerCase().endsWith("a") ||
+    word.toLowerCase().endsWith("e") ||
+    word.toLowerCase().endsWith("i") ||
+    word.toLowerCase().endsWith("o") ||
+    word.toLowerCase().endsWith("u")
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+});
+
+console.log(wordsEndingWithVowel);
 
 let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 // - Create a sumArray function that takes an array of number as a parameter, and calculate the sum of all its numbers
+
+
 
 // - Make a new array that contains number multiplied by 3 like [6, 18, 27 ...]
 
